f6e904654533609340fec2299dab1ba4850050d2
Joseph J.C. Tang <jinntrance@gmail.com>
2013-07-05 07:33:37 +0800

    inversions added

---
+++ src/Inversions.scala
@@ -0,0 +1,10 @@
+/**
+ * Copyright by <a href="http://crazyadam.net"><em><i>Joseph J.C. Tang</i></em></a> <br/>
+ * Email: <a href="mailto:jinntrance@gmail.com">jinntrance@gmail.com</a>
+ * @author joseph
+ * @since 7/4/13 9:53 PM
+ * @version 1.0
+ */
+class Inversions {
+
+}
