e61200206379951082bd749090ea8a5cc26a5b07
Joseph J.C. Tang <jinntrance@gmail.com>
2013-07-05 20:48:58 +0800

    inversions problems

--- src/Inversions.scala
+++ src/Inversions.scala
@@ -5,6 +5,32 @@
  * @since 7/4/13 9:53 PM
  * @version 1.0
  */
-class Inversions {
 
+import io.Source
+
+val ls=Source.fromFile(args(0)).getLines().toStream.map(_.toInt)
+val s1=System.currentTimeMillis()
+
+println(count(ls.toArray))
+
+def count(ls:Array[Int],answer:Long=0):Long={
+  val len=ls.length
+  if(1>=len) answer
+  else {
+    val (left,right)=ls.splitAt(len/2)
+    count(left,count(right,countMerge(left,right,answer)))
+  }
+}
+def countMerge(left:Array[Int],right:Array[Int],answer:Long):Long={
+  val (l,llen)=(left.sorted,left.length)
+  val (r,rlen)=(right.sorted,right.length)
+  var i,j,m=0
+  while(i<llen&&j<rlen){
+    if(l(i) > r(j)){
+      j+=1
+      m+=llen-i
+    }
+    else i+=1
+  }
+  answer+m
 }
